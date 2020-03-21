73896447a30661f23b746a55ab67444129752f25
Isaac Cohen <isaaclandoncohen@gmail.com>
2020-03-20 21:23:48 -0700

    more shaders

--- CreateShader.js
+++ CreateShader.js
@@ -23,13 +23,55 @@ var shaderCode = {
       gl_FragColor = vec4(vUv.x,vUv.x,vUv.x,1.);
     }`,
 
-  rightGradient:` 
+  smooth:` 
+    varying vec2 vUv;
+    void main() {
+      gl_FragColor = vec4(vUv.x,vUv.x,vUv.x,1.);
+    }`,
+
+  choppy:` 
+    varying vec2 vUv;
+    void main() {
+
+      float v = vUv.x * 10.;
+      v = mod(v ,1.);
+      gl_FragColor = vec4(v,v,v,1.);
+    }`,
+
+  random:` 
+    varying vec2 vUv;
+
+    float hash( vec2 p ){
+      float h = dot(p,vec2(127.1,311.7));
+      return -1.0 + 2.0*fract(sin(h)*43758.5453123);
+    }
+
+    void main() {
+
+      float v = hash(vec2(vUv.x));//mod(v ,1.);
+      gl_FragColor = vec4(v,v,v,1.);
+    }`,
+
+
+  sinCurves:` 
+  varying vec2 vUv;
+  void main() {
+    float r = sin( vUv.x * 20.  + .2*sin( vUv.y * 20.)) * .5 + .5;
+    float g = sin( vUv.x * 22.  + .2*sin( vUv.y * 20.))* .5 + .5;
+    float b = sin( vUv.x * 24.  + .2*sin( vUv.y * 20.))* .5 + .5;
+    gl_FragColor = vec4(r,g,b,1.);
+  }`,
+
+  sinCurves2:` 
   varying vec2 vUv;
   void main() {
     float r = sin( vUv.x * 100.  + sin( vUv.y * 20.)) * .5 + .5;
     float g = sin( vUv.y * 4.  + sin( vUv.x * 20.))* .5 + .5;
     float b = sin( vUv.x * 10.  + sin( vUv.y * 20.))* .5 + .5;
-    gl_FragColor = vec4(r,g,b,1.);
+
+    float bw = length(vec3(r,g,b));
+    bw = bw * bw * bw;
+    gl_FragColor = vec4(bw,bw,bw,1.);
   }`,
 
 
